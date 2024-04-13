eacc353fb7c36ab5db9fe8f41fc412bbb9f8b817
vijayvp09 <vvpillai95@gmail.com>
2024-04-13 09:22:38 +0530

    started portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="flex-col justify-center items-center">
+     <h1 className="font-bold text-blue-500 text-3xl">vijay v pillai</h1>
+     <h2 className="font-bold text-black text-2xl">developer</h2>
+     projects: 1: REACT image gallery with pixabay API
+    </div>
+  )
+}
+
+export default App
